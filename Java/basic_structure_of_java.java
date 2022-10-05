f6ca6665acd9a546a7a6b8543ff01d33525a6006
Satyam Gangwar <70762486+SG-07@users.noreply.github.com>
2022-10-05 16:13:36 +0530

    Create basic_structure_of_java.java

---
+++ Java/basic_structure_of_java.java
@@ -0,0 +1,17 @@
+public class Demo //class definition
+  
+{
+  public static void main(String args[])
+    
+  {
+    void display()
+    {
+      System.out.println("Welcome To GITHUB");
+    }
+  //Statements
+    
+  }
+   
+}
+
+//Footer
